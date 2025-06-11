bfd49c3211c037dfba35f0457e32a279393ee796
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2025-06-11 22:52:15 +0100

    refactor: Download pdf on mobile

--- src/components/PdfPreview.jsx
+++ src/components/PdfPreview.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { PDFViewer, pdf, usePDF } from "@react-pdf/renderer";
+import pdfImage from "../assets/pdf-image.png"
 
 import Spinner from "./Spinner";
 
@@ -24,25 +25,26 @@ const PdfPreview = ({ Doc, title, ...rest }) => {
     }
 
     return (
-        <div className="h-[90vh] w-full border border-gray-300 rounded overflow-auto">
+        <div className="md:h-[90vh] w-full p-6 border border-gray-300 rounded overflow-auto">
             <object
                 data={pdfDoc.url}
                 type="application/pdf"
                 width="100%"
                 height="100%"
             >
-                <p className="p-4">
-                    Your device does not support embedded PDFs.{" "}
+                <div className="flex flex-col gap-2 items-center">
+                    <img src={pdfImage} className="w-16" alt="" />
+                    <p className="p-4 text-center">
+                        Your device does not support embedded PDFs.
+                    </p>
                     <a
                         href={pdfDoc.url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="underline"
+                        download
+                        className="inline-block text-white bg-originalGreen px-4 py-2 rounded-md text-center"
                     >
-                        Open PDF in a new tab
+                        Open Document
                     </a>
-                    .
-                </p>
+                </div>
             </object>
         </div>
     );
